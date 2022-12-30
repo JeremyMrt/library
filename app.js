c665c020ea5f0cced8ec4a85e3f1a432fe52d15f
Jérémy Martin <114619755+JeremyMrt@users.noreply.github.com>
2022-12-30 18:17:56 +0100

    Delete function working only for starters books

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const span = document.getElementsByClassName("close")[0];
 const unreadShelf = document.getElementsByClassName("unread")[0];
 const readShelf = document.getElementsByClassName("read")[0];
 const form = document.getElementById("book-form");
+const books = document.getElementsByClassName("books");
 
+// invoked from html
 // eslint-disable-next-line no-unused-vars
 function openModal() {
   modal.style.display = "flex";
@@ -52,98 +54,119 @@ const myLibrary = [
   },
 ];
 
-function Book(title, author, pages) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+
+  addBookToLibrary() {
+    const bookDiv = document.createElement("div");
+    const bookDivTitle = document.createElement("div");
+    const bookDivAuthor = document.createElement("div");
+    const bookDivPages = document.createElement("div");
+    const bookDivButtons = document.createElement("div");
+
+    bookDiv.classList.add("books");
+    bookDivTitle.classList.add("spine-title");
+    bookDivAuthor.classList.add("spine-author");
+    bookDivPages.classList.add("spine-pages");
+    bookDivButtons.classList.add("delete-btn");
+
+    bookDivTitle.textContent = this.title;
+    bookDivAuthor.textContent = this.author;
+    bookDivPages.textContent = `${this.pages}p`;
+
+    bookDiv.append(bookDivTitle, bookDivPages, bookDivAuthor, bookDivButtons);
+
+    bookDiv.setAttribute("index", myLibrary.length - 1);
+
+    if (!this.read) unreadShelf.appendChild(bookDiv);
+    else readShelf.appendChild(bookDiv);
+  }
 }
-
-// function addingBook() {
-//   const newBook = document.createElement("div");
-
-//   const newBookTitle = document.createElement("div");
-//   newBookTitle.classList.add("spine-title");
-//   const newBookAuthor = document.createElement("div");
-//   newBookAuthor.classList.add("spine-author");
-//   const newBookPages = document.createElement("div");
-//   newBookPages.classList.add("spine-pages");
-//   const newBookButtons = document.createElement("div");
-//   newBookButtons.classList.add("delete-btn");
-
-//   newBookTitle.textContent = myLibrary[0].title;
-//   newBookAuthor.textContent = myLibrary[0].author;
-//   newBookPages.textContent = `${myLibrary[0].pages}p`;
-
-//   newBook.append(newBookTitle, newBookPages, newBookAuthor, newBookButtons);
-//   newBook.classList.add("books");
-//   newBook.setAttribute("index", "0");
-//   if (!formBookRead) unreadShelf.insertAdjacentElement("afterbegin", newBook);
-//   if (formBookRead) readShelf.insertAdjacentElement("afterbegin", newBook);
-// }
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  const formBookTitle = event.currentTarget.title.value;
-  const formBookAuthor = event.currentTarget.author.value;
-  const formBookPages = event.currentTarget.pages.value;
-  const formBookRead = event.currentTarget.read.checked;
 
-  const addbook = new Book(formBookTitle, formBookAuthor, formBookPages);
-  myLibrary.unshift(addbook);
-  // addingBook();
+  const createdBook = new Book(
+    event.currentTarget.title.value,
+    event.currentTarget.author.value,
+    event.currentTarget.pages.value,
+    event.currentTarget.read.checked
+  );
+
+  myLibrary.push(createdBook);
+  createdBook.addBookToLibrary();
 
   modal.style.display = "none";
-  event.currentTarget.title.value = "";
-  event.currentTarget.author.value = "";
-  event.currentTarget.pages.value = "";
-
-  const newBook = document.createElement("div");
-
-  const newBookTitle = document.createElement("div");
-  newBookTitle.classList.add("spine-title");
-  const newBookAuthor = document.createElement("div");
-  newBookAuthor.classList.add("spine-author");
-  const newBookPages = document.createElement("div");
-  newBookPages.classList.add("spine-pages");
-  const newBookButtons = document.createElement("div");
-  newBookButtons.classList.add("delete-btn");
-
-  newBookTitle.textContent = myLibrary[0].title;
-  newBookAuthor.textContent = myLibrary[0].author;
-  newBookPages.textContent = `${myLibrary[0].pages}p`;
-
-  newBook.append(newBookTitle, newBookPages, newBookAuthor, newBookButtons);
-  newBook.classList.add("books");
-
-  newBook.setAttribute("index", "0");
-
-  if (!formBookRead) unreadShelf.insertAdjacentElement("afterbegin", newBook);
-  else readShelf.insertAdjacentElement("afterbegin", newBook);
 });
 
 // Fake books in array for landing page
 
 function addingFakeBeginningBooks() {
   myLibrary.forEach((book) => {
-    const newBook = document.createElement("div");
+    const bookDiv = document.createElement("div");
+    const bookDivTitle = document.createElement("div");
+    const bookDivAuthor = document.createElement("div");
+    const bookDivPages = document.createElement("div");
+    const bookDivButtons = document.createElement("div");
 
-    const newBookTitle = document.createElement("div");
-    newBookTitle.classList.add("spine-title");
-    const newBookAuthor = document.createElement("div");
-    newBookAuthor.classList.add("spine-author");
-    const newBookPages = document.createElement("div");
-    newBookPages.classList.add("spine-pages");
-    const newBookButtons = document.createElement("div");
-    newBookButtons.classList.add("delete-btn");
+    bookDiv.classList.add("books");
+    bookDivTitle.classList.add("spine-title");
+    bookDivAuthor.classList.add("spine-author");
+    bookDivPages.classList.add("spine-pages");
+    bookDivButtons.classList.add("delete-btn");
 
-    newBookTitle.textContent = book.title;
-    newBookAuthor.textContent = book.author;
-    newBookPages.textContent = `${book.pages}p`;
+    bookDivTitle.textContent = book.title;
+    bookDivAuthor.textContent = book.author;
+    bookDivPages.textContent = `${book.pages}p`;
 
-    newBook.append(newBookTitle, newBookPages, newBookAuthor, newBookButtons);
-    newBook.classList.add("books");
-    unreadShelf.insertAdjacentElement("beforeend", newBook);
+    bookDiv.append(bookDivTitle, bookDivPages, bookDivAuthor, bookDivButtons);
+
+    bookDiv.setAttribute("index", myLibrary.indexOf(book));
+
+    unreadShelf.appendChild(bookDiv);
   });
 }
 
 addingFakeBeginningBooks();
+
+// if (books[0] !== undefined) {
+//   for (let i = 0; i <= books.length; i++) {
+//     const book = books[i];
+//     book.addEventListener("click", () => {
+//       myLibrary.splice(i, 1);
+//       bookshelf.removeChild(books[i]);
+//     });
+//   }
+// }
+
+// OK FOR NOW
+// Array.from(books).forEach((book) => {
+//   book.addEventListener("click", () => {
+//     myLibrary.splice(book.dataset.index, 1);
+
+//     if (book.read) readShelf.removeChild(book);
+//     else unreadShelf.removeChild(book);
+//   });
+// });
+function updateDataIndexAttributes() {
+  for (let i = 0; i < books.length; i++) {
+    books[i].setAttribute("index", i++);
+  }
+}
+
+Array.from(books).forEach((book) => {
+  book.addEventListener("click", () => {
+    console.log(book.getAttribute("index"));
+    myLibrary.splice(book.getAttribute("index"), 1);
+
+    if (book.read) readShelf.removeChild(book);
+    else unreadShelf.removeChild(book);
+
+    updateDataIndexAttributes();
+  });
+});
